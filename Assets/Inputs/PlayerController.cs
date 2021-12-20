d70164c044209ae129c6c3224ae0a905c09620d7
dsol_cpu <dmsolinsky@email.wm.edu>
2021-12-20 00:52:26 -0500

    this new input system needs to check out soon

--- Assets/Inputs/PlayerController.cs
+++ Assets/Inputs/PlayerController.cs
@@ -3,182 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float moveSpeed = .015f;
-    [SerializeField] private float jumpHeight = 0f;
-    [SerializeField] private float gravityValue = -9.81f;   
-//    private CharacterController controller;
     private PlayerInput playerControls;
-    Vector3 movementInput = Vector3.zero;
-    Vector3 playerVelocity;
-    bool groundedPlayer;
-    bool jumped = false;
-    bool isRunning = false;
+    private Vector2 movementInput;
+    public float verticalAxis;
+    public float horizontalAxis;
 
-    void Awake()
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody rb;
+    private float movementSpeed = 7f;
+    private float rotationSpeed = 15;
+    private Vector3 targetDirection = Vector3.zero;
+
+
+    private void Awake()
     {
-        playerControls = new PlayerInput();
-        //controller = gameObject.GetComponent<CharacterController>();
-        playerControls.Player.Move.performed += ctx => OnMove(ctx);
-        playerControls.Player.Move.canceled += ctx => movementInput = Vector3.zero;
-        playerControls.Player.Jump.performed += ctx => OnJump(ctx);
-        playerControls.Player.Jump.canceled += ctx => jumpHeight = 0f;
+        rb = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
     }
 
     private void OnEnable()
     {
-        playerControls.Player.Enable();
+        if(playerControls == null)
+        {
+            playerControls = new PlayerInput();
+            playerControls.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+            playerControls.Player.Move.canceled += ctx => movementInput = Vector2.zero;
+        }
+        playerControls.Enable();
     }
     private void OnDisable()
     {
         playerControls.Disable();
     }
 
-    void FixedUpdate()
+    private void HandleMovementInput()
     {
-        transform.Translate(movementInput * Time.deltaTime);
-
-        //could probably just make it a coordinate space and multiply speed with (0,1), (1,0), etc. and then cap speed
-
-        if(Input.GetKey("w"))
-        {
-            movementInput.z += moveSpeed;
-        }
-
-        if (Input.GetKey("a"))
-        {
-            movementInput.x -= moveSpeed;
-        }
-
-        if (Input.GetKey("s"))
-        {
-            movementInput.z -= moveSpeed;
-        }
-
-        if (Input.GetKey("d"))
-        {
-            movementInput.x += moveSpeed;
-        }
-        
-        if(Input.GetKeyDown("shift"))
-        {
-            isRunning = true;
-        }
-
-        if (Input.GetKeyDown("shift"))
-        {
-            isRunning = false;
-        }
-
-        if (!isRunning)
-        {
-            switch (movementInput) //yikes
-            {
-                case Vector3 v when v.x > 5f:
-                    movementInput.x = 5f;
-                    break;
-                case Vector3 v when v.x < -5f:
-                    movementInput.x = -5f;
-                    break;
-                case Vector3 v when v.z > 5f:
-                    movementInput.z = 5f;
-                    break;
-                case Vector3 v when v.z < -5f:
-                    movementInput.z = -5f;
-                    break;
-
-                case Vector3 v when v.x > 5f && v.z > 5f:
-                    movementInput.x = 5f;
-                    movementInput.z = 5f;
-                    break;
-                case Vector3 v when v.x < -5f && v.z < -5f:
-                    movementInput.x = -5f;
-                    movementInput.z = -5f;
-                    break;
-                case Vector3 v when v.x > 5f && v.z < -5f:
-                    movementInput.x = 5f;
-                    movementInput.z = -5f;
-                    break;
-                case Vector3 v when v.x < -5f && v.z > 5f:
-                    movementInput.x = -5f;
-                    movementInput.z = 5f;
-                    break;
-            }
-        }
-        if (isRunning)
-        {
-            switch (movementInput) //yikes
-            {
-                case Vector3 v when v.x > 7f:
-                    movementInput.x = 7f;
-                    break;
-                case Vector3 v when v.x < -7f:
-                    movementInput.x = -7f;
-                    break;
-                case Vector3 v when v.z > 7f:
-                    movementInput.z = 7f;
-                    break;
-                case Vector3 v when v.z < -7f:
-                    movementInput.z = -7f;
-                    break;
-
-                case Vector3 v when v.x > 7f && v.z > 7f:
-                    movementInput.x = 7f;
-                    movementInput.z = 7f;
-                    break;
-                case Vector3 v when v.x < -7f && v.z < -7f:
-                    movementInput.x = -7f;
-                    movementInput.z = -7f;
-                    break;
-                case Vector3 v when v.x > 7f && v.z < -7f:
-                    movementInput.x = 7f;
-                    movementInput.z = -7f;
-                    break;
-                case Vector3 v when v.x < -7f && v.z > 7f:
-                    movementInput.x = -7f;
-                    movementInput.z = 7f;
-                    break;
-            }
-        }
-
-        //controller.Move(playerVelocity * Time.deltaTime);
+        verticalAxis = movementInput.y;
+        horizontalAxis = movementInput.x;
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void HandleAllInput()
     {
-        movementInput.x = context.ReadValue<Vector2>().normalized.x;
-        movementInput.z = context.ReadValue<Vector2>().normalized.y;
-
-        Debug.Log("i LIVE!");
-    }
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
+        HandleMovementInput();
     }
 
-    public void OnFire(InputAction.CallbackContext context)
+    private void HandleMovement()
     {
-        Debug.Log("Yay fire pew pew");
+        moveDirection = cameraObject.forward * verticalAxis + cameraObject.right * horizontalAxis;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection *= movementSpeed;
+        rb.velocity = moveDirection;
     }
 
-    public void OnJump(InputAction.CallbackContext context)
+    private void HandleRotation()
     {
-        jumped = context.ReadValue<bool>();
-        jumped = context.action.triggered;
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        throw new System.NotImplementedException();
+        targetDirection = cameraObject.forward * verticalAxis + cameraObject.right * horizontalAxis;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
 
-    }
-    public void OnTriggerExit(Collider other)
-    {
-        throw new System.NotImplementedException();
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
+        transform.rotation = playerRotation;
     }
 
+    private void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void Update()
+    {
+        HandleAllInput();
+    }
+    private void FixedUpdate()
+    {
+        HandleAllMovement();
+    }
 
 }
